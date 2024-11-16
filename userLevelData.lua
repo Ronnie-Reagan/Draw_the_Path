bd915a90dda5ed7b00d9e4787e6184a8da601460
Don Reagan <147023412+Ronnie-Reagan@users.noreply.github.com>
2024-11-16 16:58:49 -0700

    Add files via upload

---
+++ userLevelData.lua
@@ -0,0 +1,15 @@
+local userLevelData = {
+    [1] = {floors = {}, obstacles = {}, walls = {}, playerCharacter = 
+    {
+    x = 100,
+    y = 100,
+    size = 25,
+    radius = 25,
+    friction = 2,
+    mass = 25,
+    rotationalPower = 5000,
+    gravitationalPower = 1}
+    }
+}
+
+return userLevelData
